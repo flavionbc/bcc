f42196cfe043a4af94b0379e12905e712026de5e
alexbrigido <alexvbrigido@hotmail.com>
2015-09-11 21:28:37 -0300

    Ajustando tela de clientes

--- src/main/java/com/paysecure/bcc/mb/ClienteMB.java
+++ src/main/java/com/paysecure/bcc/mb/ClienteMB.java
@@ -8,31 +8,37 @@ import javax.faces.bean.ManagedBean;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.paysecure.bcc.client.impl.ClienteRestImpl;
 import com.paysecure.bcc.dto.Cliente;
-
+import com.paysecure.bcc.enums.StatusClienteEnum;
+import com.paysecure.bcc.util.JsfUtil;
 
 @Controller
 @ManagedBean(name="clienteMB")
 //@Scope("view")
 public class ClienteMB {
 
+	Logger log = Logger.getLogger(ClienteMB.class);
+	
 	private @Getter @Setter Cliente cliente;
 	private @Getter @Setter List<Cliente> clientes;
 	
 	private ClienteRestImpl service;
 	
-	@RequestMapping(value="/clientes.xhtml", method=RequestMethod.GET)
 	public void buscar(){
+		log.info("Buscando clientes...");
 		cliente = new Cliente();
 		cliente.setNomeFantasia("BANCO ALEXANDRE SA");
 		cliente.setId(1L);
+		cliente.setUf("DF");
+		cliente.setStatusCliente(StatusClienteEnum.ATIVO);
 		clientes = new ArrayList<Cliente>();
 		clientes.add(cliente);
+		JsfUtil.redirecionarUsuario("/clientes.xhtml");
 	}
 	
 	public void cadastrar(){
