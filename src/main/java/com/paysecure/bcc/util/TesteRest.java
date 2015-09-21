6ec480554c193a7d8b9a854ba0a1f08b25767db4
alexbrigido <alexvbrigido@hotmail.com>
2015-09-21 17:43:05 -0300

    atualizando classe de teste

--- src/main/java/com/paysecure/bcc/util/TesteRest.java
+++ src/main/java/com/paysecure/bcc/util/TesteRest.java
@@ -1,19 +1,22 @@
 package com.paysecure.bcc.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import com.paysecure.bcc.client.AbstractRest;
-import com.paysecure.bcc.dto.Cliente;
-import com.paysecure.bcc.enums.StatusClienteEnum;
+import com.paysecure.bcc.dto.Funcionalidade;
+import com.paysecure.bcc.dto.Perfil;
+import com.paysecure.bcc.dto.Usuario;
+import com.paysecure.bcc.enums.StatusUsuarioEnum;
 import com.paysecure.bcc.enums.UrlEnum;
 
 public class TesteRest extends AbstractRest {
-
 	
 	public static void main(String[] args) {
 		new TesteRest().cadastrar();
@@ -22,23 +25,36 @@ public class TesteRest extends AbstractRest {
 	private void cadastrar(){
 	
 		try{
-			Cliente c= new Cliente();
-			c.setNomeFantasia("BANCO ALEXANDRE SA");
-			c.setCep("70000000");
-			c.setNroEmissor("999");
-			c.setStatusCliente(StatusClienteEnum.ATIVO);
-			c.setUf("DF");
-			
+			Usuario c= new Usuario();
+			c.setNome("ALEXANDRE DIAS");
+			c.setDataCriacao(new Date());;
+			c.setSenha("123");
+			c.setCpf("123");
+			c.setStatus(StatusUsuarioEnum.ATIVO);;
+			c.setLogin("alexandre.dias");
+			c.setPerfil(getPerfis());
 			Map<String, String> vars = new HashMap<String, String>();
 	        vars.put("id", "JS01");
 	        
 			RestTemplate rt = getRestTemplate();
-			rt.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
-            rt.getMessageConverters().add(new StringHttpMessageConverter());
-			String url = getUrl(UrlEnum.CLIENTE_URL.getUrl(), "insert");
-			rt.postForObject(url, c, Cliente.class);
+			String url = getUrl(UrlEnum.USUARIO_URL.getUrl(), "insert");
+			rt.postForObject(url, c, Usuario.class);
 		}catch(Exception ex){
 			ex.printStackTrace();
 		}
+	}
+	
+	private Perfil getPerfis(){
+		Funcionalidade f = new Funcionalidade();
+		f.setDescricao("TUDO");
+		f.setId(1);
+		Perfil p = new Perfil();
+		p.setDescricao("ADMIN");
+		p.setId(1);
+		p.setFuncionalidades(new ArrayList<Funcionalidade>(Arrays.asList(f)));
+		List<Perfil> per = new ArrayList<Perfil>();
+		per.add(p);
+		
+		return p;
 	}
 }
